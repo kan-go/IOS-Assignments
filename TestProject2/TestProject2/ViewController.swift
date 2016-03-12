c64f0f16f32d35948f6192d4de86e20784940f91
kan-go <kanikagoela@yahoo.com>
2016-03-12 12:41:12 +0800

    Project 2
    Testing

---
+++ TestProject2/TestProject2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject2
+//
+//  Created by Kanika Goela on 12/3/2016.
+//  Copyright © 2016 Kanika Goela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
