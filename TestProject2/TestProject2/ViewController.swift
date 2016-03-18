fe05a15d46c2ba9b8f4e87bd2f2002f9743b6015
kan-go <kanikagoela@yahoo.com>
2016-03-18 15:04:02 +0800

    Session 3 practice
    Session 3 practice

--- TestProject2/TestProject2/ViewController.swift
+++ TestProject2/TestProject2/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBAction func buttonAction(sender: AnyObject) {
+        
+        var servings:Int = 2
+        
+        // Better to put Int or String when assigning values to variables. Xcode lets you do it without Int or String. It guesses based on the first value you assign
+        
+        print(servings)
+        print(servings*5)
+        
+        var name:String = "Kanika"
+        
+        print(name)
+        
+        var greeting:String = "Hello " + name
+        
+        print(greeting)
+        
+        var greeting2:String = "hello"
+        
+        print(greeting2+" "+name)
+        
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
